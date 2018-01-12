9e681504e8921344bcc3a0ccb63abffd31d68ee2
Ethan Chen <ethan.cfchen@gmail.com>
2018-01-12 14:47:21 +0800

    Refactoring

--- generators/app/templates/gulp-tasks/build/extras.js
+++ generators/app/templates/gulp-tasks/build/extras.js
@@ -1,35 +1,75 @@
+const fs = require('fs');
+const _ = require('lodash');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const through = require('through2');
+const config = require('config');
 
-const _ = require('lodash');
+const ASSETS = {
+  manifest: 'package.json',
+};
 
-const setup = require('setup/setup');
+function getVersion() {
+  const assets = config.assets;
+  const file = assets.manifest || ASSETS.manifest;
+  const content = fs.readFileSync(file, 'utf8');
+  const manifest = JSON.parse(content);
+  return manifest.version;
+}
 
 function noop() {
   return through.obj();
 }
 
+function getFilterOptions() {
+  return ((config.preprocess || {}).filter || {}).extras;
+}
+
+function getPreprocessOptions() {
+  const globals = (config.globals || {}).extras;
+  const options = {};
+  const defaults = {
+    context: {
+      ENV: config.env,
+      VERSION: getVersion(),
+      DOMAIN: config.domain,
+    },
+  };
+
+  _.merge(defaults.context, globals);
+  _.merge(options, defaults);
+  if (!globals) {
+    return false;
+  }
+  return options;
+}
+
+function getOptions() {
+  return {
+    filter: getFilterOptions(),
+    preprocess: getPreprocessOptions(),
+  };
+}
+
 module.exports = function() {
   const browserSync = this.context.browserSync;
 
-  const assets = setup.assets;
-  const preprocessOpts = setup.plugins.gulpPreprocess;
-
-  const extrasData = (setup.globals || {}).extras;
-  const isPreprocess = !_.isEmpty(extrasData);
-  const filterOpts = preprocessOpts.filter.extras;
-  const $filter = filterOpts ? $.filter(filterOpts, {restore: true}) : noop();
-  const $filterRestore = filterOpts ? $filter.restore : noop();
+  const assets = config.assets;
 
   const src = assets.src.extras;
-  const dest = setup.root;
+  const dest = config.root;
 
-  preprocessOpts.context = _.merge(preprocessOpts.context, extrasData);
+  const options = getOptions();
+  const isPreprocess = !_.isEmpty(options.preprocess);
+
+  const $filter = options.filter ?
+    $.filter(options.filter, {restore: true}) : noop();
+  const $filterRestore = options.filter ?
+    $filter.restore : noop();
 
   return gulp.src(src, {cwd: assets.base.src})
     .pipe($.if(isPreprocess, $filter))
-    .pipe($.if(isPreprocess, $.preprocess(preprocessOpts)))
+    .pipe($.if(isPreprocess, $.preprocess(options.preprocess)))
     .pipe($.if(isPreprocess, $filterRestore))
     .pipe(gulp.dest(dest, {cwd: assets.build}))
     .pipe(browserSync.stream());
--- generators/app/templates/gulp-tasks/build/scripts.js
+++ generators/app/templates/gulp-tasks/build/scripts.js
@@ -1,65 +1,109 @@
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const through = require('through2');
-
-const path = require('path');
-const _ = require('lodash');
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
+const config = require('config');
 
-const setup = require('setup/setup');
+const ASSETS = {
+  manifest: 'package.json',
+};
+
+function getVersion() {
+  const assets = config.assets;
+  const file = assets.manifest || ASSETS.manifest;
+  const content = fs.readFileSync(file, 'utf8');
+  const manifest = JSON.parse(content);
+  return manifest.version;
+}
 
 function noop() {
   return through.obj();
 }
 
+function getFilterOptions() {
+  return ((config.preprocess || {}).filter || {}).js;
+}
+
+function getPreprocessOptions() {
+  const globals = (config.globals || {}).js;
+  const options = {};
+  const defaults = {
+    context: {
+      ENV: config.env,
+      VERSION: getVersion(),
+      DOMAIN: config.domain,
+    },
+  };
+
+  _.merge(defaults.context, globals);
+  _.merge(options, defaults);
+  return options;
+}
+
+function getWebpackOptions() {
+  const assets = config.assets;
+  const isWebpack = fs.existsSync(assets.config.webpack);
+  let options = null;
+  if (isWebpack) {
+    options = require(assets.config.webpack);
+    if (_.isFunction(options)) {
+      options = options(config);
+    }
+  }
+  return options;
+}
+
+function getOptions() {
+  return {
+    filter: getFilterOptions(),
+    preprocess: getPreprocessOptions(),
+    uglify: config.uglify || {},
+    webpack: getWebpackOptions(),
+  };
+}
+
 module.exports = function() {
   const browserSync = this.context.browserSync;
 
-  const assets = setup.assets;
+  const assets = config.assets;
+  const isOnline = config.isOnline;
+  const isVerbose = config.isVerbose;
 
   const src = assets.src.scripts;
-  const dest = path.join(setup.root, assets.dest.scripts);
+  const dest = path.join(config.root, assets.dest.scripts);
 
-  const isWebpack = assets.isFileExist(assets.config.webpack);
-  const isBabel = assets.isFileExist(assets.config.babel);
+  const isBabel = fs.existsSync(assets.config.babel);
 
-  let webpackOpts = null;
-  const uglifyOpts = setup.uglify || {};
-  const preprocessOpts = setup.plugins.gulpPreprocess;
+  const options = getOptions();
 
-  const jsData = (setup.globals || {}).js;
-  const filterOpts = preprocessOpts.filter.js;
-  const $filter = filterOpts ? $.filter(filterOpts, {restore: true}) : noop();
-  const $filterRestore = filterOpts ? $filter.restore : noop();
+  const $filter = options.filter ?
+    $.filter(options.filter, {restore: true}) : noop();
+  const $filterRestore = options.filter ?
+    $filter.restore : noop();
 
-  preprocessOpts.context = _.merge(preprocessOpts.context, jsData);
-
-  if (isWebpack) {
-    webpackOpts = require(assets.config.webpack);
-
-    if (_.isFunction(webpackOpts)) {
-      webpackOpts = webpackOpts(setup);
-    }
-
+  if (options.webpack) {
     return gulp.src(src, {cwd: assets.base.src})
-      .pipe($.if(!setup.isOnline, $.plumber()))
-      .pipe(webpackStream({config: webpackOpts}, webpack))
+      .pipe($.if(!isOnline, $.plumber()))
+      .pipe(webpackStream({config: options.webpack}, webpack))
       .pipe(gulp.dest(dest, {cwd: assets.build}))
       .pipe(browserSync.stream());
   }
 
   return gulp.src(src, {cwd: assets.base.src})
-    .pipe($.if(!setup.isOnline, $.plumber()))
+    .pipe($.if(!isOnline, $.plumber()))
     .pipe($filter)
-    .pipe($.preprocess(preprocessOpts))
+    .pipe($.preprocess(options.preprocess))
     .pipe($filterRestore)
-    .pipe($.if(setup.isVerbose, $.sourcemaps.init()))
+    .pipe($.if(isVerbose, $.sourcemaps.init()))
     .pipe($.if(isBabel, $.babel()))
     .pipe($.include())
-    .pipe($.if(setup.isOnline, $.uglify(uglifyOpts)))
+    .pipe($.if(isOnline, $.uglify(options.uglify)))
     .on('error', (err) => console.error(err))
-    .pipe($.if(setup.isVerbose, $.sourcemaps.write()))
+    .pipe($.if(isVerbose, $.sourcemaps.write()))
     .pipe(gulp.dest(dest, {cwd: assets.build}))
     .pipe(browserSync.stream());
 };
